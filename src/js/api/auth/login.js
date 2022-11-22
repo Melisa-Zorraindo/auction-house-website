c50f3ed226a38f74fe598890fb34439162e2661a
Melisa-Zorraindo <melisa.zorraindo@gmail.com>
2022-11-22 14:31:39 +0100

    add login functionality

---
+++ src/js/api/auth/login.js
@@ -0,0 +1,41 @@
+import { API_AUCTION_URL } from "../constants.js";
+import { displayApiError } from "../../errorHandling/apiError.js";
+import * as storage from "../../storage/index.js";
+
+const LOGIN_PATH = "auth/login";
+
+/**
+ * Sends a POST request to the API
+ * to log in an existing user
+ * @param {email} email
+ * @param {string} password
+ */
+export async function login(email, password) {
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json; charset=UTF-8",
+    },
+    body: JSON.stringify({
+      email,
+      password,
+    }),
+  };
+
+  const response = await fetch(`${API_AUCTION_URL}${LOGIN_PATH}`, options);
+  const result = await response.json();
+
+  const USER_FEEDBACK = document.querySelector("#feedback");
+
+  if (response.ok) {
+    const { accessToken, credits, ...profile } = result;
+    storage.saveToStorage("accessToken", accessToken);
+    storage.saveToStorage("credits", credits);
+    storage.saveToStorage("profile", profile);
+  } else {
+    const {
+      errors: [{ message }],
+    } = result;
+    displayApiError(USER_FEEDBACK, message);
+  }
+}
