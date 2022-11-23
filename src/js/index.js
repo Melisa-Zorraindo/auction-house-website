231b5aa51e49d9d7ee25d08fab9b3860a2967933
Melisa-Zorraindo <melisa.zorraindo@gmail.com>
2022-11-23 14:37:09 +0100

    fix bug
    log out functionality is available only if there's a profile saved in local storage

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,15 @@
 import { renderNavbarDesktop } from "./components/navbar/desktopNavbar.js";
 import { renderNavbarMobile } from "./components/navbar/mobileNavbar.js";
+import { loadFromStorage } from "./storage/load.js";
 import { logout } from "./handlers/logout.js";
 
+const profile = loadFromStorage("profile");
+
+//render navigation bars
 renderNavbarDesktop();
 renderNavbarMobile();
-logout();
+
+//make log out functionaliy available only if there's a profile saved in storage
+if (profile) {
+  logout();
+}
