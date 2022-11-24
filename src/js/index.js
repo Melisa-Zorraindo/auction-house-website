fa759ec746de624a2b4cddbbf8022fbc4b17cdfe
Melisa-Zorraindo <melisa.zorraindo@gmail.com>
2022-11-24 13:45:54 +0100

    refactor

--- src/js/index.js
+++ src/js/index.js
@@ -2,8 +2,7 @@ import { renderNavbarDesktop } from "./components/navbar/desktopNavbar.js";
 import { renderNavbarMobile } from "./components/navbar/mobileNavbar.js";
 import { loadFromStorage } from "./storage/load.js";
 import { logout } from "./handlers/logout.js";
-import { createListingsHTML } from "./components/listings/listings.js";
-import { fetchListings } from "./api/feed/read.js";
+import { renderHomepage } from "./handlers/renderHomepage.js";
 
 const profile = loadFromStorage("profile");
 
@@ -16,8 +15,5 @@ if (profile) {
   logout();
 }
 
-//render listings on homepage
-const LATEST_ITEMS = await fetchListings();
-
-const LATEST_LISTINGS = document.querySelector("#latest-listings");
-createListingsHTML(LATEST_LISTINGS, "Latest listings", LATEST_ITEMS);
+//render homepage
+renderHomepage();
