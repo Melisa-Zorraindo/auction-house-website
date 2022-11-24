172bfece5bfb4805147ae80a3dd52b72cbc16f83
Melisa-Zorraindo <melisa.zorraindo@gmail.com>
2022-11-24 15:42:42 +0100

    rearrange components rendering

--- src/js/handlers/renderHomepage.js
+++ src/js/handlers/renderHomepage.js
@@ -1,15 +1,18 @@
 import { fetchListings } from "../api/feed/read.js";
 import { sortByMostPopular } from "../components/listings/mostPopular.js";
 import { createListingsHTML } from "../components/listings/latestListings.js";
+import { getFinishSoonItems } from "../components/listings/finishSoon.js";
 
 export async function renderHomepage() {
-  const LATEST_ITEMS = await fetchListings();
+  const MOST_POPULAR = await sortByMostPopular();
+  const MOST_POPULAR_LISTINGS = document.querySelector("#popular-listings");
+  createListingsHTML(MOST_POPULAR_LISTINGS, "Most popular", MOST_POPULAR);
 
+  const LATEST_ITEMS = await fetchListings();
   const LATEST_LISTINGS = document.querySelector("#latest-listings");
   createListingsHTML(LATEST_LISTINGS, "Latest listings", LATEST_ITEMS);
 
-  const MOST_POPULAR = await sortByMostPopular();
-
-  const MOST_POPULAR_LISTINGS = document.querySelector("#popular-listings");
-  createListingsHTML(MOST_POPULAR_LISTINGS, "Most popular", MOST_POPULAR);
+  const FINISH_SOON = await getFinishSoonItems();
+  const FINISH_SOON_ITEMS = document.querySelector("#finish-soon");
+  createListingsHTML(FINISH_SOON_ITEMS, "Finish soon", FINISH_SOON);
 }
