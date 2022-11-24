319f601724f35b1c266b9f59dc3af0bd8427439d
Melisa-Zorraindo <melisa.zorraindo@gmail.com>
2022-11-24 18:28:01 +0100

    refactor

--- src/js/index.js
+++ src/js/index.js
@@ -1,19 +1,4 @@
-import { renderNavbarDesktop } from "./components/navbar/desktopNavbar.js";
-import { renderNavbarMobile } from "./components/navbar/mobileNavbar.js";
-import { loadFromStorage } from "./storage/load.js";
-import { logout } from "./handlers/logout.js";
 import { renderHomepage } from "./handlers/renderHomepage.js";
-
-const profile = loadFromStorage("profile");
-
-//render navigation bars
-renderNavbarDesktop();
-renderNavbarMobile();
-
-//make log out functionaliy available only if there's a profile saved in storage
-if (profile) {
-  logout();
-}
 
 //render homepage
 renderHomepage();
