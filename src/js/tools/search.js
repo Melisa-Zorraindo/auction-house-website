3edfd9f001620b7a4d1f0a1a5ef21ac6e8e63037
Melisa-Zorraindo <melisa.zorraindo@gmail.com>
2022-11-25 18:27:49 +0100

    update jsdocs

--- src/js/tools/search.js
+++ src/js/tools/search.js
@@ -3,16 +3,18 @@ import { createListingsHTML } from "../components/listings/allListings.js";
 /**
  * Uses the string passed in by the user
  * in the search bar to filter posts which
- * include the string either in the body
- * or in the username
+ * include the string either in the listing
+ * title or in its description
  * @param {Array} array to implement filter on
  * @param {string} queryString
  * @param {HTMLDivElement} container where to render the filtered elements
  */
 export function searchListings(arr, queryString, container) {
-  const filteredPosts = arr.filter(({ title, description, tags }) => {
-    return title.toLowerCase().includes(queryString.toLowerCase());
-    // ||  description.toLowerCase().includes(queryString.toLowerCase())
+  const filteredPosts = arr.filter(({ title, description }) => {
+    return (
+      title.toLowerCase().includes(queryString.toLowerCase()) ||
+      description.toLowerCase().includes(queryString.toLowerCase())
+    );
   });
 
   container.innerHTML = "";
