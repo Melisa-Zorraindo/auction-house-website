91016c2cbad0c9c30ff0ae5fe74083def8886359
Melisa-Zorraindo <melisa.zorraindo@gmail.com>
2022-11-25 11:08:33 +0100

    update API error text and <p> location

--- src/js/errorHandling/apiError.js
+++ src/js/errorHandling/apiError.js
@@ -6,5 +6,5 @@ export function displayApiError(container, apiError) {
   USER_FEEDBACK.classList.remove("d-none");
   USER_FEEDBACK.classList.add("bg-danger");
   USER_FEEDBACK.classList.add("text-light");
-  USER_FEEDBACK.innerHTML = apiError;
+  USER_FEEDBACK.innerHTML = `An error occurred. Please try again later.</br> Error type: ${apiError}`;
 }
