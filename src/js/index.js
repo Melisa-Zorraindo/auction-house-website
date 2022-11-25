b53fe8cc5efa475471d45dc0272815c84ca29c18
Melisa-Zorraindo <melisa.zorraindo@gmail.com>
2022-11-25 11:09:49 +0100

    refactor

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,13 @@
 import { renderHomepage } from "./handlers/renderHomepage.js";
+import { renderAllListingsPage } from "./handlers/renderAllListingsPage.js";
 
-//render homepage
-renderHomepage();
+//render different pages
+const path = location.pathname;
+
+if (path === "/index.html") {
+  //render homepage
+  renderHomepage();
+} else if (path === "/all-listings.html") {
+  //render all listings page
+  renderAllListingsPage();
+}
