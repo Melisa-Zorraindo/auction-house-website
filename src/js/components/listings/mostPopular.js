76cd6e9aaf569cc3e2e9d90439a5097875895bbf
Melisa-Zorraindo <melisa.zorraindo@gmail.com>
2022-11-27 09:36:41 +0100

    hide past bids

--- src/js/components/listings/mostPopular.js
+++ src/js/components/listings/mostPopular.js
@@ -2,5 +2,19 @@ import { fetchListings } from "../../api/feed/read.js";
 
 export async function sortByMostPopular() {
   const ALL_ITEMS = await fetchListings();
-  return ALL_ITEMS.sort((a, b) => b._count.bids - a._count.bids);
+
+  ALL_ITEMS.sort((a, b) => b._count.bids - a._count.bids);
+
+  //discard finished bids (earlier than today)
+  const today = new Date();
+
+  let array = [];
+  ALL_ITEMS.map((item) => {
+    const newDate = new Date(item.endsAt);
+    if (newDate >= today) {
+      array.push(item);
+    }
+  });
+
+  return array;
 }
