c8e4afcc3ff8e5e134f1ba853b7ec3f6b84af220
Melisa-Zorraindo <melisa.zorraindo@gmail.com>
2022-11-28 09:46:08 +0100

    remove unused API call

--- src/js/api/feed/read.js
+++ src/js/api/feed/read.js
@@ -2,7 +2,6 @@ import { API_AUCTION_URL } from "../constants.js";
 import { displayApiError } from "../../errorHandling/apiError.js";
 
 let LISTINGS_PATH = "listings?sort=created&sortOrder=desc";
-let LISTINGS_PAINTING_PATH = "listings?_tag=Painting";
 
 /**
  * Sends a GET request to the API
@@ -15,28 +14,6 @@ export async function fetchListings() {
   };
 
   const response = await fetch(`${API_AUCTION_URL}${LISTINGS_PATH}`, options);
-  const result = await response.json();
-
-  if (response.ok) {
-    return result;
-  } else {
-    const {
-      errors: [{ message }],
-    } = result;
-    const USER_FEEDBACK = document.querySelector("#listings-feedback");
-    displayApiError(USER_FEEDBACK, message);
-  }
-}
-
-export async function fetchPaintings() {
-  const options = {
-    method: "GET",
-  };
-
-  const response = await fetch(
-    `${API_AUCTION_URL}${LISTINGS_PAINTING_PATH}`,
-    options
-  );
   const result = await response.json();
 
   if (response.ok) {
