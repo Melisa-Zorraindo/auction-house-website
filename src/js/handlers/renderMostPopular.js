93c5c56b15c6c796f7c5b106caa36b376dd02311
Melisa-Zorraindo <melisa.zorraindo@gmail.com>
2022-11-30 18:40:04 +0100

    fix bug

--- src/js/handlers/renderMostPopular.js
+++ src/js/handlers/renderMostPopular.js
@@ -29,7 +29,7 @@ export async function renderMostPopularPage() {
   selectAvatar();
 
   //create new listing functionality
-  createListingsHTML();
+  createNewListing();
 
   //render footer
   createFooterHTML();
